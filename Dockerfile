a7245928e31f3e9169be26ca76fdc00e169c1c52
19h <kenan@sly.mn>
2022-04-04 15:10:29 +0200

    Automatically determine latest runner version using GH tag api

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,6 @@ FROM styla/ghar-node-base:latest
 ENV AGENT_TOOLSDIRECTORY=/opt/hostedtoolcache
 RUN mkdir -p /opt/hostedtoolcache
 
-ARG GH_RUNNER_VERSION="2.288.1"
 ARG TARGETPLATFORM
 
 SHELL ["/bin/bash", "-o", "pipefail", "-c"]
--- install_actions.sh
+++ install_actions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
-GH_RUNNER_VERSION=$1
 TARGETPLATFORM=$2
+
+export GH_RUNNER_VERSION_LATEST=$(curl https://api.github.com/repos/actions/runner/tags | grep -m 1 name | sed -e 's/^.*name.*: "//' | sed -e 's/",.*$//')
 
 export TARGET_ARCH="x64"
 if [[ $TARGETPLATFORM == "linux/arm/v7" ]]; then
@@ -9,7 +10,7 @@ elif [[ $TARGETPLATFORM == "linux/arm64" ]]; then
     export TARGET_ARCH="arm64"
 fi
 
-curl -L "https://github.com/actions/runner/releases/download/v${GH_RUNNER_VERSION}/actions-runner-linux-${TARGET_ARCH}-${GH_RUNNER_VERSION}.tar.gz" >actions.tar.gz
+curl -L "https://github.com/actions/runner/releases/download/v${GH_RUNNER_VERSION_LATEST}/actions-runner-linux-${TARGET_ARCH}-${GH_RUNNER_VERSION_LATEST}.tar.gz" >actions.tar.gz
 
 tar -zxf actions.tar.gz
 rm -f actions.tar.gz
